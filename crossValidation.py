b076d91e32864c7801e0d6fcb4da2d04bb65ced4
Gustavo Vieira <guustavov@gmail.com>
2019-03-18 13:49:50 -0300

    started cross validation module with functions to split dataset in k-folds (default k = 10)

---
+++ crossValidation.py
@@ -0,0 +1,54 @@
+import numpy as np
+import pandas as pd
+import fileUtils as f
+import auxiliaryLog
+from sklearn.model_selection import StratifiedKFold
+
+def createFolds(datasetPath, foldsPath):
+    dataset = pd.read_csv(datasetPath)
+
+    f.createDirectory(foldsPath)
+
+    classFeatureName = dataset.columns[len(dataset.columns) - 1]
+
+    # removing all instances that have no class value
+    dataset = dataset.dropna(subset=[classFeatureName])
+
+    dataset = binarizeDataset(dataset, classFeatureName)
+
+    folds = splitDataset(dataset, classFeatureName, 10)
+
+    # using only 10% of the original dataset
+    folds = splitDataset(folds[0], classFeatureName, 10)
+
+    for index, fold in enumerate(folds):
+        writeFoldToCsv(fold, index, foldsPath)
+
+def binarizeDataset(dataset, classFeatureName):
+    benignFilter = dataset[classFeatureName] == "BENIGN"
+    notBenignFilter = dataset[classFeatureName] != "BENIGN"
+
+    dataset.loc[benignFilter, classFeatureName] = 0
+    dataset.loc[notBenignFilter, classFeatureName] = 1
+    
+    return dataset
+
+def splitDataset(dataset, classFeatureName, numberOfSplits = 10):
+    names = dataset.columns
+
+    x = dataset.drop([classFeatureName], axis = 1) # all instances with no class feature
+    y = getattr(dataset, classFeatureName).values # class feature of all instances
+
+    splitter = StratifiedKFold(n_splits = 10)
+
+    folds = []
+    for indexes in splitter.split(x, y):
+        folds.append(pd.DataFrame(dataset.values[indexes[1],], columns = names))
+
+    return folds
+
+def writeFoldToCsv(fold, foldIndex, destinationPath):
+    foldFilename = "fold_" + str(foldIndex) + ".csv"
+    fold.to_csv(destinationPath + foldFilename, index = False)
+
+    auxiliaryLog.log(foldFilename + ' saved')

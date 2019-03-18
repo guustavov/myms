ba5896ea82f9779748da2586fa04dfd1b72a9a81
Gustavo Vieira <guustavov@gmail.com>
2019-03-18 13:45:48 -0300

    auxiliary log refactor

--- auxiliaryLog.py
+++ auxiliaryLog.py
@@ -1,7 +1,5 @@
 import datetime
 import inspect
 
-class auxlog:
-    @staticmethod
-    def log(message):
-        print('[' + str(datetime.datetime.now()).split('.')[0] + '] ' + message)
+def log(message):
+    print('[' + str(datetime.datetime.now()).split('.')[0] + '] ' + message)

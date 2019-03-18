0f7403a690be3771f0f78cc83874b35dd7fe9471
Gustavo Vieira <guustavov@gmail.com>
2019-03-18 13:46:37 -0300

    use auxiliary log instead of print()

--- fileUtils.py
+++ fileUtils.py
@@ -1,11 +1,11 @@
-import pickle, datetime, os
+import pickle, datetime, os, auxiliaryLog
 
 def saveModelToFile(model, path, cvIteration, prefix = ''):
     createDirectory(path)
 
     fileName = path + prefix + str(cvIteration)
     pickle.dump(model, open(fileName, 'wb'))
-    print('[' + str(datetime.datetime.now()).split('.')[0] + '] ' + fileName + ' saved')
+    auxiliaryLog.log(fileName + ' saved')
 
 def createDirectory(directoryPath):
     if not os.path.exists(directoryPath):

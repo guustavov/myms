ba843cfad658dceb4a12eb932bd3b38f1484ea73
Gustavo <guustavov@gmail.com>
2019-11-19 07:15:05 -0300

    WIP: draft of softmax ANN implementation

--- myms.py
+++ myms.py
@@ -1,25 +1,38 @@
 import auxiliaryLog
 from multiprocessing import Process, Pool, TimeoutError
-import sys, time, os
+import sys
+import time
+import os
 import pandas as pd
 import fileUtils as f
 import crossValidation as cv
 
 from hybrid import OriginalHybrid
+from ann import SoftmaxANN
 
 from sklearn.datasets.samples_generator import make_classification
 from sklearn import metrics
 
+from keras.models import Sequential
+from keras.layers.core import Dense
+from keras.callbacks import EarlyStopping
+
+
+def runSoftmax():
+    softmax = SoftmaxANN()
+    cv.run(softmax, '/home/gstav/projects/cicids2017/1-percent/')
+
 def multiRunHybrid(args):
-    runHybrid(*args)
+    runSoftmax(*args)
 
 def runHybrid(superiorLimit, inferiorLimit):
     originalHybrid = OriginalHybrid(superiorLimit, inferiorLimit)
     cv.run(originalHybrid, '/media/gstav/Data/github/cicids2017/1-percent/')
 
+
 if __name__ == '__main__':
     pool = Pool(processes=8)
-    
+
     # x, Y = make_classification(n_samples=100, n_features=23, n_classes=2)
     # allHybridConfigs = [
     #     (x, Y, 0, 25),
@@ -32,7 +45,6 @@ if __name__ == '__main__':
     #     (x, Y, 25, 100)
     # ]
 
-
     allHybridConfigs = [
         (0, 25),
         (0, 50),
@@ -44,4 +56,5 @@ if __name__ == '__main__':
         (25, 100)
     ]
 
-    pool.map(multiRunHybrid, allHybridConfigs)
+    # pool.map(multiRunHybrid, allHybridConfigs)
+    runSoftmax()

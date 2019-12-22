d6d9c69fe4936c881fe8920f029a571c2bc8d82e
Gustavo <guustavov@gmail.com>
2019-12-22 01:09:35 -0300

    #9 Changed CV fold creation (keep label as nominal and generate both binary and multiclass subsets) and refactor CV run method (organization of iteration artifacts and metrics)

--- crossValidation.py
+++ crossValidation.py
@@ -11,93 +11,97 @@ from sklearn import metrics
 import os, os.path
 
 def createFolds(datasetPath, foldsPath):
-    dataset = pd.read_csv(datasetPath)
+	dataset = pd.read_csv(datasetPath)
+	
+	multiclassPath = '{}multiclass/'.format(foldsPath)
+	binaryPath = '{}binary/'.format(foldsPath)
 
-    f.createDirectory(foldsPath)
+	f.createDirectory(multiclassPath)
+	f.createDirectory(binaryPath)
 
-    classFeatureName = dataset.columns[len(dataset.columns) - 1]
+	classFeatureName = dataset.columns[len(dataset.columns) - 1]
 
-    # removing all instances that have no class value
-    dataset = dataset.dropna(subset=[classFeatureName])
+	# removing all instances that have no class value
+	dataset = dataset.dropna(subset=[classFeatureName])
 
-    dataset = binarizeDataset(dataset, classFeatureName)
+	folds = splitDataset(dataset, classFeatureName, 10)
 
-    folds = splitDataset(dataset, classFeatureName, 10)
+	for index, fold in enumerate(folds):
+		f.saveFoldToCsv(fold, index, multiclassPath)
 
-    for index, fold in enumerate(folds):
-        f.saveFoldToCsv(fold, index, foldsPath)
+		binarizedFold = binarizeDataset(fold, classFeatureName)
+		f.saveFoldToCsv(binarizedFold, index, binaryPath)
 
 def binarizeDataset(dataset, classFeatureName):
-    benignFilter = dataset[classFeatureName] == "BENIGN"
-    notBenignFilter = dataset[classFeatureName] != "BENIGN"
+	notBenignFilter = dataset[classFeatureName] != 'BENIGN'
 
-    dataset.loc[benignFilter, classFeatureName] = 0
-    dataset.loc[notBenignFilter, classFeatureName] = 1
-    
-    return dataset
+	dataset.loc[notBenignFilter, classFeatureName] = 'ATTACK'
+	
+	return dataset
 
 def splitDataset(dataset, classFeatureName, numberOfSplits = 10):
-    names = dataset.columns
+	names = dataset.columns
 
-    x = dataset.drop([classFeatureName], axis = 1) # all instances with no class feature
-    y = getattr(dataset, classFeatureName).values # class feature of all instances
+	x = dataset.drop([classFeatureName], axis = 1) # all instances with no class feature
+	y = getattr(dataset, classFeatureName).values # class feature of all instances
 
-    splitter = StratifiedKFold(numberOfSplits)
+	splitter = StratifiedKFold(numberOfSplits)
 
-    folds = []
-    for indexes in splitter.split(x, y):
-        folds.append(pd.DataFrame(dataset.values[indexes[1],], columns = names))
+	folds = []
+	for indexes in splitter.split(x, y):
+		folds.append(pd.DataFrame(dataset.values[indexes[1],], columns = names))
 
-    return folds
+	return folds
 
 def run(model, foldsPath):
 	numberOfFolds = len([name for name in os.listdir(foldsPath) if os.path.isfile(os.path.join(foldsPath, name))])
 
-	modelClassName = (model.__class__.__name__) 
-					#+ str(model.superiorLimit) + "_" 
-					#+ str(model.inferiorLimit))
+	modelClassName = model.getName()
+
 	pathToPersistModels = foldsPath + modelClassName + '/'
 
-	if (isinstance(model, SoftmaxANN)):
-		for iteration in range(0,numberOfFolds):
-			if (os.path.isfile(pathToPersistModels + 'results/result_' + str(iteration))):
-				auxiliaryLog.log('skipped ' + modelClassName + ' [iteration ' + str(iteration) + ']')
-				continue
+	columns = ['acc', 'pre', 'rec', 'f1']
+	if isinstance(model, OriginalHybrid):
+		columns.append('reclassified')
 
-			trainFolds = glob.glob(foldsPath + 'fold_[!' + str(iteration) + ']*.csv')
-			trainData = pd.concat((pd.read_csv(fold) for fold in trainFolds))
-			testData = pd.read_csv(foldsPath + "fold_" + str(iteration) + ".csv")
+	resultMetricsDataFrame = pd.DataFrame(columns=columns)
 
-			train_x, train_Y = splitXY(trainData)
-			test_x, test_Y = splitXY(testData)
+	for iteration in range(0,numberOfFolds):
+		trainFolds = glob.glob(foldsPath + 'fold_[!' + str(iteration) + ']*.csv')
+		trainData = pd.concat((pd.read_csv(fold) for fold in trainFolds))
+		testData = pd.read_csv(foldsPath + "fold_" + str(iteration) + ".csv")
 
-			history = model.fit(train_x, train_Y)
+		train_x, train_Y = splitXY(trainData)
+		test_x, test_Y = splitXY(testData)
 
-			f.createDirectory(pathToPersistModels)
-			f.saveModelToFile(model, pathToPersistModels, iteration)
-			# f.saveModelToFile(model.knn.model, pathToPersistModels, iteration)
+		history = model.fit(train_x, train_Y)
 
-			predictions = model.predict(test_x)
+		predictions = model.predict(test_x)
+		auxiliaryLog.log('performed prediction of {} [iteration {}]'.format(modelClassName, str(iteration)))
 
-			predictions = np.argmax(predictions, axis = 1)
+		resultMetrics = {
+			'acc': metrics.accuracy_score(test_Y, predictions),
+			'pre': metrics.precision_score(test_Y, predictions),
+			'rec': metrics.recall_score(test_Y, predictions),
+			'f1': metrics.f1_score(test_Y, predictions)
+		}
 
-			accuracy_score = metrics.accuracy_score(test_Y, predictions)
-			precision_score = metrics.precision_score(test_Y, predictions)
-			recall_score = metrics.recall_score(test_Y, predictions)
-			f1_score = metrics.f1_score(test_Y, predictions)
-			confusion_matrix = metrics.confusion_matrix(test_Y, predictions)
-                        
-			auxiliaryLog.log("performed prediction of " + modelClassName + "[iteration" + str(iteration) + "]")
-			result = ('acc: ' + str(accuracy_score)
-				+ '\npre: ' + str(precision_score)
-				+ '\nrec: ' + str(recall_score)
-				+ '\nf1: ' + str(f1_score)
-				+ '\n' + 'matrix: ' + str(confusion_matrix))
+		if isinstance(model, OriginalHybrid):
+			resultMetrics.reclassified = '{}%'.format(model.percentageOfReclassified)
 
-			if hasattr(model, 'percentageOfReclassified'):
-				result = result + '\nreclassified: ' + str(model.percentageOfReclassified) + '%'
+		resultMetricsDataFrame = resultMetricsDataFrame.append(resultMetrics, ignore_index=True)
 
-			f.saveResultToFile(result, pathToPersistModels + "results_softmax/", iteration)
+		confusion_matrix = metrics.confusion_matrix(test_Y, predictions)
+
+		iterationArtifacts = {
+			'model': model,
+			'history': history,
+			'confusion_matrix': confusion_matrix
+		}
+
+		f.saveIterationArtifactsToFile(iterationArtifacts, pathToPersistModels, iteration)
+
+	f.saveResultToFile(resultMetricsDataFrame, pathToPersistModels)
 	
 def splitXY(data):
 	if(isinstance(data, pd.DataFrame)):
